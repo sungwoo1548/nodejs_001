39bd6775f0583f550df1542654a8b68dc3718c37
sungwoo1548 <swo1548@gmail.com>
2019-07-21 02:10:18 +0900

    jquery cdn + button action @ index.ejs

--- test1/app.js
+++ test1/app.js
@@ -14,10 +14,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public'))); // public폴더 접근 순서 올리기..  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
